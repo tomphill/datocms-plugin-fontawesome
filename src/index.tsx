5e4c9c2e5c72a94ab093f8fcc484a16333b262fa
tom <tomphill@live.co.uk>
2022-04-02 12:05:06 +0800

    add migration code

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import {
   connect,
   IntentCtx,
+  OnBootCtx,
   RenderFieldExtensionCtx,
 } from "datocms-plugin-sdk";
 import { render } from "./utils/render";
@@ -35,6 +36,39 @@ if (process.env.NODE_ENV === "development") {
         default:
           return null;
       }
+    },
+    async onBoot(ctx: OnBootCtx) {
+      // if we already performed the migration, skip
+      if (ctx.plugin.attributes.parameters.migratedFromLegacyPlugin) {
+        return;
+      }
+
+      // if the current user cannot edit fields' settings, skip
+      if (!ctx.currentRole.meta.final_permissions.can_edit_schema) {
+        return;
+      }
+
+      // get all the fields currently associated to the plugin...
+      const fields = await ctx.loadFieldsUsingPlugin();
+
+      // ... and for each of them...
+      await Promise.all(
+        fields.map(async (field: any) => {
+          // set the fieldExtensionId to be the new one
+          await ctx.updateFieldAppearance(field.id, [
+            {
+              operation: "updateEditor",
+              newFieldExtensionId: "fontawesome",
+            },
+          ]);
+        })
+      );
+
+      // save in configuration the fact that we already performed the migration
+      ctx.updatePluginParameters({
+        ...ctx.plugin.attributes.parameters,
+        migratedFromLegacyPlugin: true,
+      });
     },
   });
 }
