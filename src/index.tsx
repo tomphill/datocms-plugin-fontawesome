cbcec562c5e5e8aabca8e4352e25a42356fdfd41
tom <tomphill@live.co.uk>
2022-04-02 15:27:54 +0800

    remove reactdom

--- src/index.tsx
+++ src/index.tsx
@@ -7,68 +7,57 @@ import {
 import { render } from "./utils/render";
 import "datocms-react-ui/styles.css";
 import FontAwesomePicker from "./components/FontAwesomePicker";
-import ReactDOM from "react-dom";
 
-if (process.env.NODE_ENV === "development") {
-  const container = document.createElement("div");
-  document.body.appendChild(container);
+connect({
+  manualFieldExtensions(ctx: IntentCtx) {
+    return [
+      {
+        id: "fontawesome",
+        name: "Font Awesome",
+        type: "editor",
+        fieldTypes: ["json"],
+      },
+    ];
+  },
+  renderFieldExtension(fieldExtensionId: string, ctx: RenderFieldExtensionCtx) {
+    switch (fieldExtensionId) {
+      case "fontawesome":
+        return render(<FontAwesomePicker ctx={ctx} />);
+      default:
+        return null;
+    }
+  },
+  async onBoot(ctx: OnBootCtx) {
+    // if we already performed the migration, skip
+    if (ctx.plugin.attributes.parameters.migratedFromLegacyPlugin) {
+      return;
+    }
 
-  ReactDOM.render(<FontAwesomePicker />, container);
-} else if (process.env.NODE_ENV === "production") {
-  connect({
-    manualFieldExtensions(ctx: IntentCtx) {
-      return [
-        {
-          id: "fontawesome",
-          name: "Font Awesome",
-          type: "editor",
-          fieldTypes: ["json"],
-        },
-      ];
-    },
-    renderFieldExtension(
-      fieldExtensionId: string,
-      ctx: RenderFieldExtensionCtx
-    ) {
-      switch (fieldExtensionId) {
-        case "fontawesome":
-          return render(<FontAwesomePicker ctx={ctx} />);
-        default:
-          return null;
-      }
-    },
-    async onBoot(ctx: OnBootCtx) {
-      // if we already performed the migration, skip
-      if (ctx.plugin.attributes.parameters.migratedFromLegacyPlugin) {
-        return;
-      }
+    // if the current user cannot edit fields' settings, skip
+    if (!ctx.currentRole.meta.final_permissions.can_edit_schema) {
+      return;
+    }
 
-      // if the current user cannot edit fields' settings, skip
-      if (!ctx.currentRole.meta.final_permissions.can_edit_schema) {
-        return;
-      }
+    // get all the fields currently associated to the plugin...
+    const fields = await ctx.loadFieldsUsingPlugin();
 
-      // get all the fields currently associated to the plugin...
-      const fields = await ctx.loadFieldsUsingPlugin();
+    // ... and for each of them...
+    await Promise.all(
+      fields.map(async (field: any) => {
+        // set the fieldExtensionId to be the new one
+        await ctx.updateFieldAppearance(field.id, [
+          {
+            operation: "updateEditor",
+            newFieldExtensionId: "fontawesome",
+          },
+        ]);
+      })
+    );
 
-      // ... and for each of them...
-      await Promise.all(
-        fields.map(async (field: any) => {
-          // set the fieldExtensionId to be the new one
-          await ctx.updateFieldAppearance(field.id, [
-            {
-              operation: "updateEditor",
-              newFieldExtensionId: "fontawesome",
-            },
-          ]);
-        })
-      );
-
-      // save in configuration the fact that we already performed the migration
-      ctx.updatePluginParameters({
-        ...ctx.plugin.attributes.parameters,
-        migratedFromLegacyPlugin: true,
-      });
-    },
-  });
-}
+    // save in configuration the fact that we already performed the migration
+    ctx.updatePluginParameters({
+      ...ctx.plugin.attributes.parameters,
+      migratedFromLegacyPlugin: true,
+    });
+  },
+});
